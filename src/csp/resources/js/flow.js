6725ae2bfdcf7a8f031368678af559e999705413
Henry Hamon <henry.pereira@bplus.com.br>
2022-05-29 16:08:01 -0300

    ✨ Flow2Code, create production

--- src/csp/resources/js/flow.js
+++ src/csp/resources/js/flow.js
@@ -193,12 +193,13 @@ const loadFlowMenu = (data) => {
     }
 
 const handleExport = () => {
+  const prod_name = document.getElementById('production-name').value;
   const jsn = editor.export();
   if (!jsn.drawflow) return;
   if (!jsn.drawflow.Home) return;
   const flow = {};
   flow.nodes = [];
-  flow.name = "Production1";
+  flow.name = prod_name;
   production = jsn.drawflow.Home.data;
   Object.getOwnPropertyNames(production).forEach(el => {
     let element = production[el],
@@ -232,6 +233,28 @@ const handleExport = () => {
   });
 
   console.log(flow);
+  postData('/csp/irisflow/api/generate', flow)
+    .then(data => {
+      console.log(data); // JSON data parsed by `data.json()` call
+    });
+}
+
+async function postData(url = '', data = {}) {
+  // Default options are marked with *
+  const response = await fetch(url, {
+    method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    mode: 'cors', // no-cors, *cors, same-origin
+    cache: 'no-cache',
+    credentials: 'same-origin',
+    headers: {
+      'Content-Type': 'application/json'
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    redirect: 'follow', // manual, *follow, error
+    referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    body: JSON.stringify(data) // body data type must match "Content-Type" header
+  });
+  return response.json(); // parses JSON response into native JavaScript objects
 }
 
 document.onreadystatechange = function(event) {
